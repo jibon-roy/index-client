69de0499a9a33dca7ab1aeef8310f99f44d0d0f3
jibon-roy <mailme.jibon@gmail.com>
2023-12-28 22:42:29 +0600

    navbar added in sticky position

--- src/layouts/main/MainLayout.jsx
+++ src/layouts/main/MainLayout.jsx
@@ -1,9 +1,12 @@
+import { Outlet } from "react-router-dom";
+import NavBar from "./navBar/NavBar";
 
 
 const MainLayout = () => {
     return (
         <div>
-
+            <NavBar></NavBar>
+            <Outlet></Outlet>
         </div>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      "primary-white": "#F5F5F5",
+      "primary-blue": "#1C98ED",
+      "primary-black": "#333333",
+      "secondary-blue": "#176FAE",
+      "secondary-white": "#F0F2F5",
+      "white": "#FFFFFF"
+    },
     extend: {},
   },
   plugins: [require('daisyui')],
